c976b3a24b1bbb5c15a381e747070e5f92a7074e
carme <carme@192.168.1.133>
2018-03-21 18:44:12 +0100

    Initial commit

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,44 @@
 package it.polito.tdp.spellchecker.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
 public class SpellCheckerController {
-	
+
+    @FXML
+    private MenuButton menuLanguage;
+
+    @FXML
+    private TextField txtInserted;
+
+    @FXML
+    private Button btnSpell;
+
+    @FXML
+    private TextField txtResult;
+
+    @FXML
+    private Label labelError;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Label labelTime;
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+
+    }
+
 }
+
